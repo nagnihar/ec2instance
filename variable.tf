8f7e710d94e6b29fbdbd97e0b43664b1d3289451
nagnihar <nagnihar143@gmail.com>
2025-05-31 13:10:00 -0400

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+
+variable "instance_name" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "ec2-tf-instance"
+}
+
+
